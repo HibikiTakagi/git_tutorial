48c2bb61a8d274897f0f69ee07b04e431908cd50
HibikiTakagi <takagihbk@acsl.ics.keio.ac.jp>
2024-03-18 18:04:29 +0900

    4th commit @main

--- hello.c
+++ hello.c
@@ -2,5 +2,6 @@
 
 void main ( void ) {
   printf("Hello, world!\n");
-  printf("This is the first commit for git tutorial!\n");
+  printf("This is the first commit for git tutorial!\n");]
+  printf("Good night @main branch\n");
 }
